8f610a82c74d6fdab0459f9d84bc1f3d6daccf34
Víctor López Juan <victor@lopezjuan.com>
2014-03-08 22:28:19 +0100

    Documentation and example for Coiterative comonad

--- src/Control/Comonad/Trans/Coiter.hs
+++ src/Control/Comonad/Trans/Coiter.hs
@@ -23,9 +23,14 @@ module Control.Comonad.Trans.Coiter
   (
   -- |
   -- Coiterative comonads represent non-terminating, productive computations.
+  --
+  -- They are the dual notion of iterative monads. While iterative computations
+  -- produce no values or eventually terminate with one, coiterative
+  -- computations constantly produce values and they never terminate.
   -- 
   -- It's simpler form, 'Coiter', is an infinite stream of data. 'CoiterT'
-  -- generalizes this notion to an arbitrary comonad.
+  -- extends this so that each step of the computation can be performed in
+  -- a comonadic context.
     CoiterT(..)
   -- * The coiterative comonad
   , Coiter, coiter, runCoiter
@@ -33,6 +38,8 @@ module Control.Comonad.Trans.Coiter
   , unfold
   -- * Cofree comonads
   , ComonadCofree(..)
+  -- * Example
+  -- $example
   ) where
 
 import Control.Arrow
@@ -149,3 +156,92 @@ coiterTDataType :: DataType
 coiterTDataType = mkDataType "Control.Comonad.Trans.Coiter.CoiterT" [coiterTConstr]
 {-# NOINLINE coiterTDataType #-}
 #endif
+
+{- $example
+Many numerical approximation methods compute infinite sequences of results; each,
+hopefully, more accurate than the previous one.
+
+<https://en.wikipedia.org/wiki/Newton's_method Newton's method>
+to find zeroes of a function is one such algorithm.
+ 
+@ \{\-\# LANGUAGE FlexibleInstances, MultiParamTypeClasses, UndecidableInstances \#\-\} @
+
+> {-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, UndecidableInstances #-}
+
+> import Control.Comonad.Trans.Coiter
+> import Control.Comonad.Env
+> import Control.Applicative
+> import Data.Foldable (toList, find)
+
+> data Function = Function { function   :: Double -> Double,
+>                            derivative :: Double -> Double
+>                          }
+> 
+> data Result = Result { value  :: Double,
+>                        xerror :: Double,
+>                        ferror :: Double
+>                        } deriving (Show)
+> 
+> data Outlook = Outlook { result :: Result,
+>                          progress :: Bool } deriving (Show)
+> 
+
+To make our lives easier, we will store the problem at hand using the Env
+environment comonad.
+
+> type Solution a = CoiterT (Env Function) a
+
+Accessing the environment when working with the stream is simple enough:
+ 
+> instance ComonadEnv e w => ComonadEnv e (CoiterT w) where
+>   ask (CoiterT w) = ask w
+
+We can express an iterative algorithm using unfold over a problem and
+an initial value.
+ 
+> newton :: Env Function Double -> Solution Double
+> newton = unfold (\wd ->
+>                     let  f  = asks function wd in
+>                     let df  = asks derivative wd in
+>                     let  x  = extract wd in
+>                     x - (f x)/(df x))
+> 
+> 
+
+To estimate the error, we look forward one position in the stream. The next value
+will be much more precise than the current one, so we can consider it as the
+actual result.
+
+> estimateError :: Solution Double -> Result
+> estimateError s =
+>   let a:a':_ = toList s in
+>   let f = asks function s in
+>   Result { value = a,
+>            xerror = abs $ a - a',
+>            ferror = abs $ f a
+>          }
+
+To get a sense of when the algorithm is making any progress, we can sample the
+future and check if the result improves at all.
+ 
+> estimateOutlook :: Int -> Solution Result -> Outlook
+> estimateOutlook sampleSize solution =
+>   let sample = map ferror $ take sampleSize $ tail $ toList solution in
+>   Outlook { result = extract solution,
+>             progress = (ferror (extract solution) > foldl1 min sample) } 
+
+To compute the square root of @c@, we solve the equation @x*x - c = 0@.
+
+> squareRoot :: Double -> Maybe Result
+> squareRoot c = let problem = flip env c (Function { function = (\x -> x*x - c),
+>                                                     derivative = (\x -> 2*x) })
+>                in 
+>                fmap result $ find (not . progress) $ 
+>                  newton problem =>> estimateError =>> estimateOutlook 5
+
+This program will output the result together with the error.
+
+> main :: IO ()
+> main = putStrLn $ show $ (squareRoot 4)
+
+-}
